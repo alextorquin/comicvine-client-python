744f4c02b53fa598e072cfb1b4be0145bdff033d
Gitlab CI <>
2020-06-01 13:15:12 +0000

    ci: bundle spec during validation

--- git_push.sh
+++ git_push.sh
@@ -24,7 +24,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="ci: validate spec with swagger-cli and openapi-generator
+    release_note="ci: bundle spec during validation
 "
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
