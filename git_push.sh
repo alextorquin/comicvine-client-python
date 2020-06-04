87cdcae893332457557e8330b10320e16ed507ca
Gitlab CI <>
2020-06-04 13:42:39 +0000

    ci: dereference spec

--- git_push.sh
+++ git_push.sh
@@ -24,7 +24,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="feat: define parameters in main file
+    release_note="ci: dereference spec
 "
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
